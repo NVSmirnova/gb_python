5659e4ac9deee83f9073e5ed9e7e515d2d2575a7
NVSmirnova <smirnova10nv@gmail.com>
2021-08-18 15:52:28 +0300

    1st homework upd

--- homeWork_1.py
+++ homeWork_1.py
@@ -1,38 +1,38 @@
 import time
 
 # task 1
-#print("task 1")
-#a = int(input("Введите целое число: "))
-#name = str(input("Введите имя: "))
-#print("My name is", name)
-#print("I am ", a, "years old.")
+print("task 1")
+a = int(input("Введите целое число: "))
+name = str(input("Введите имя: "))
+print("My name is", name)
+print("I am ", a, "years old.")
 
 # task2
-#print("task 2")
-#x = int(input("Введите количество секунд (каждые 86400 секунд счетчик обнуляется): "))
-#print(time.strftime('%H:%M:%S', time.gmtime(x)))
+print("task 2")
+x = int(input("Введите количество секунд (каждые 86400 секунд счетчик обнуляется): "))
+print(time.strftime('%H:%M:%S', time.gmtime(x)))
 
 #task 3
-#print("task 3")
-#n = input("Введите целое число: ")
-#nn = int(n + n)
-#nnn = int(n + n + n)
-#n = int(n)
-#print(n, "+", nn, "+", nnn, "=", n+nn+nnn)
+print("task 3")
+n = input("Введите целое число: ")
+nn = int(n + n)
+nnn = int(n + n + n)
+n = int(n)
+print(n, "+", nn, "+", nnn, "=", n+nn+nnn)
 
 #task 4
-#print("task 4")
-#m = input("Введите целое положительное число: ")
-#i = 0
-#otv = int(m[i])
-#while i < (len(m)):
-#    if int(m[i]) > otv:
-#        otv = int(m[i])
-#    i = i + 1
-#print(otv)
+print("task 4")
+m = input("Введите целое положительное число: ")
+i = 0
+otv = int(m[i])
+while i < (len(m)):
+    if int(m[i]) > otv:
+        otv = int(m[i])
+    i = i + 1
+print(otv)
 
 #task 5
-#print("task 5")
+print("task 5")
 vyr = int(input("Укажите выручку: "))
 izd = int(input("Укажите издержки: "))
 if vyr > izd:
@@ -46,7 +46,7 @@ else:
     print("У вас только убытки")
 
 #task 6
-#print("task 6")
+print("task 6")
 nach_km = int(input("Укажите начальную дистанцию: "))
 kon_km = int(input("Укажите конечную дистанцию: "))
 den = 1
